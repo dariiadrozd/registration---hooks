18d3cfa993ab6548e98d0e6bd0a06c767be9fd8c
dariiadrozd <darriiadrozd@gmail.com>
2024-02-08 18:11:10 +0100

    satrt

--- registration-hooks/src/page/RegistartionPage/RegistartionPage.js
+++ registration-hooks/src/page/RegistartionPage/RegistartionPage.js
@@ -14,10 +14,14 @@ function RegistartionPage() {
 
     function saveData() {
         try {
-            if (!/^[\w\d]+@[\w]+\.[\w]{2,4}$/gm.test(inp.email)) 
-            throw new Error('Email введен неверно');
-            if (inp.password != inp.confirm) throw new Error('Пароли не совпадают')
+            if (!inp.password || !inp.email || !inp.confirm)
+                throw new Error('есть незаполненные поля')
+            if (!/^[\w\d]+@[\w]+\.[\w]{2,4}$/gm.test(inp.email))
+                throw new Error('Email введен неверно');
+            if (inp.password != inp.confirm)
+                throw new Error('Пароли не совпадают')
             console.log(inp);
+            setInp({ email: "", password: "", confirm: "" })
         } catch (error) {
             console.log(error.message);
         }
@@ -25,35 +29,42 @@ function RegistartionPage() {
 
     return (
         <>
-            <h1>Registration</h1>
+            <h1>Login</h1>
             <div>
                 <TextField
-                    onChange={collectionInfo1}
+                    onChange={checkVal}
                     name="email"
                     id="outlined-basic"
                     label="Email"
-                    variant="outlined">
-                </TextField>
-            </div>
-
-            {/* <div>
-                <TextField
-                    onChange={collectionInfo2}
-                    name="password"
-                    id="outlined-basic"
-                    label="Password"
-                    variant="outlined">
+                    variant="outlined"
+                    value={inp.email}>
                 </TextField>
             </div>
 
             <div>
                 <TextField
-                    onChange={collectionInfo3}
-                    namr=""
-                    id="outlined-basic" label="ConfirmPassword" variant="outlined"></TextField>
-            </div> */}
+                    onChange={checkVal}
+                    name="password"
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    value={inp.password}>
 
-            <Button onClick={doClick} variant="outlined">Login</Button>
+                </TextField>
+            </div>
+
+            <div>
+                <TextField
+                    onChange={checkVal}
+                    name="confirm"
+                    id="outlined-basic"
+                    label="ConfirmPassword"
+                    variant="outlined"
+                    value={inp.confirm}>
+                </TextField>
+            </div>
+
+            <Button onClick={saveData} variant="outlined">Login</Button>
         </>
     )
 }
